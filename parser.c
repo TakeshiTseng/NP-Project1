642703090cd1e778f2cbbb25335c15b95a1edac4
Takeshi <a86487817@gmail.com>
2014-10-21 23:01:05 +0800

    finish parser

--- parser.c
+++ parser.c
@@ -1,47 +1,53 @@
 #include "parser.h"
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include "token.h"
+#include "tokenizer.h"
+#include "cmd_node.h"
 
-int _next_state(int state, token_node_t* token_list);
+int _next_state(int state, int next_token_type);
+void _insert_arg(arg_node_t** arg_list, arg_node_t* node);
 
-void parse(token_node_t* token_list, cmd_node_t** node_list) {
+
+cmd_node_t* parse_tokens() {
 
     int state = STATE_INIT;
     int pre_state = STATE_INIT;
     cmd_node_t* current_cmd_node = NULL;
+    cmd_node_t* cmd_node_list = NULL;
 
-    while(token_list != NULL) {
+    // token
+    int token_type;
+    char* token_str;
+
+    token_type = next_token(&token_str);
+    while(token_type != NEW_LINE) {
         if(state == STATE_INIT){
             if(current_cmd_node == NULL) {
-                state = _next_state(state, token_list);
+                state = _next_state(state, token_type);
             } else {
                 // pipe
-                token_node_t* pipe_node = token_list;
-                if(pipe_node->type == PIPE) {
+                if(token_type == PIPE) {
                     current_cmd_node->pipe_count = 1;
                 } else {
+                    // pipt n
                     // cut first letter
                     char* pipe_n_str;
-                    pipe_n_str = malloc(sizeof(pipe_node->token_str));
-                    strcpy(pipe_n_str, (pipe_node->token_str+1));
-                    int pipe_n = atoi(pipe_n_str);
-                    current_cmd_node->pipe_count = pipe_n;
+                    pipe_n_str = malloc(sizeof(token_str));
+                    strcpy(pipe_n_str, (token_str+1));
+                    // set number to pipe count
+                    current_cmd_node->pipe_count = atoi(pipe_n_str);
+                    free(pipe_n_str);
                 }
 
-                token_node_t* tmp_node = token_list;
-                token_list = token_list->next_node;
-
-                // release unused node
-                tmp_node->next_node = NULL;
-                free(tmp_node);
-
-                insert_cmd_node(node_list, current_cmd_node);
+                insert_cmd_node(&cmd_node_list, current_cmd_node);
                 current_cmd_node = NULL;
 
+                // get next token
+                token_type = next_token(&token_str);
                 // change to next state
                 pre_state = state;
-                state = _next_state(state, token_list);
+                state = _next_state(state, token_type);
             }
         } else if(state == STATE_CMD) {
             // from init state
@@ -53,110 +59,119 @@ void parse(token_node_t* token_list, cmd_node_t** node_list) {
             current_cmd_node->next_node = NULL;
 
             // copy text to cmd node
-            current_cmd_node->cmd = malloc(sizeof(char) * (strlen(token_list->token_str) + 1));
-            strcpy(current_cmd_node->cmd, token_list->token_str);
-
+            current_cmd_node->cmd = malloc(sizeof(token_str));
+            strcpy(current_cmd_node->cmd, token_str);
 
             // change to next node
-            token_node_t* tmp_token_node = token_list;
-            token_list = token_list->next_node;
+            token_type = next_token(&token_str);
 
             // change state
             pre_state = state;
-            state = _next_state(state, token_list);
+            state = _next_state(state, token_type);
 
-            // release memory
-            tmp_token_node->next_node = NULL;
-            free(tmp_token_node);
         } else if(state == STATE_ARGS) {
             // from cmd
             // calculate how many arguments
-            token_node_t* tmp_node = token_list;
+            arg_node_t* arg_list = NULL;
             int count = 0;
-            while(tmp_node != NULL && tmp_node->type == CMD) {
+            while(token_type == CMD) {
+                arg_node_t* arg = malloc(sizeof(arg_node_t));
+                arg->arg_str = malloc(sizeof(token_str));
+                _insert_arg(&arg_list, arg);
+                token_type = next_token(&token_str);
                 count++;
-                tmp_node = tmp_node->next_node;
             }
+
             current_cmd_node->args = malloc(sizeof(char*) * count);
 
             count = 0;
-            while(token_list != NULL && token_list->type == CMD) {
-                // copy string to args
-                current_cmd_node->args[count] = malloc(sizeof(char) * (strlen(token_list->token_str) + 1));
-                strcpy(current_cmd_node->args[count], token_list->token_str);
 
-                // change to next node
-                token_node_t* tmp_token_node = token_list;
-                token_list = token_list->next_node;
+            while(arg_list != NULL) {
+                current_cmd_node->args[count] = malloc(sizeof(arg_list->arg_str));
+                strcpy(current_cmd_node->args[count], arg_list->arg_str);
+                count++;
 
-                // release previous node
-                tmp_token_node->next_node = NULL;
-                free(tmp_token_node);
+                // release unused node
+                arg_node_t* node_to_release = arg_list;
+                arg_list = arg_list->next_node;
+                node_to_release->next_node = NULL;
+                free(node_to_release);
             }
 
             pre_state = state;
-            state = _next_state(state, token_list);
+            state = _next_state(state, token_type);
         } else if(state == STATE_FILE) {
-
             // release ">" token node
-            token_node_t* tmp_node = token_list;
-            token_list = token_list->next_node;
-            tmp_node->next_node = NULL;
-            free(tmp_node);
+            token_type = next_token(&token_str);
+            current_cmd_node->pipe_to_file = 1;
+            current_cmd_node->filename = malloc(sizeof(token_str));
+            strcpy(current_cmd_node->filename, token_str);
 
-            current_cmd_node->filename = malloc(sizeof(char)*(strlen(token_list->token_str) + 1));
-            strcpy(current_cmd_node->filename, token_list->token_str);
 
-            tmp_node = token_list;
-            token_list = token_list->next_node;
-
-            // release unused node
-            tmp_node->next_node = NULL;
-            free(tmp_node);
-
+            token_type = next_token(&token_str);
             pre_state = state;
-            state = _next_state(state, token_list);
+            state = _next_state(state, token_type);
         }
     }
+    // new line
+    if(pre_state == STATE_INIT) {
+        // do nothing?
+    } else if(pre_state == STATE_FILE || pre_state == STATE_ARGS || pre_state == STATE_CMD) {
+        insert_cmd_node(&cmd_node_list, current_cmd_node);
+    }
+    return cmd_node_list;
 }
 
 
 
-int _next_state(int state, token_node_t* token_list) {
-    int type = token_list->type;
+int _next_state(int state, int next_token_type) {
 
     if(state == STATE_INIT) {
-        if(type == CMD) {
+        if(next_token_type == CMD) {
             return STATE_CMD;
-        } else if(type == NEW_LINE) {
+        } else if(next_token_type == NEW_LINE) {
             return STATE_NW;
         }
     } else if(state == STATE_CMD) {
-        if(type == CMD) {
+        if(next_token_type == CMD) {
             return STATE_ARGS;
-        } else if(type == PASS) {
+        } else if(next_token_type == PASS) {
             return STATE_FILE;
-        } else if(type == NEW_LINE) {
+        } else if(next_token_type == NEW_LINE) {
             return STATE_NW;
-        } else if(type == PIPE || type == PIPE_N) {
+        } else if(next_token_type == PIPE || next_token_type == PIPE_N) {
             return STATE_INIT;
         }
     } else if(state == STATE_ARGS) {
-        if(type == CMD) {
+        if(next_token_type == CMD) {
             return STATE_ARGS;
-        } else if(type == PIPE || type == PIPE_N) {
+        } else if(next_token_type == PIPE || next_token_type == PIPE_N) {
             return STATE_INIT;
-        } else if(type == PASS) {
+        } else if(next_token_type == PASS) {
             return STATE_FILE;
-        } else if(type == NEW_LINE) {
+        } else if(next_token_type == NEW_LINE) {
             return STATE_NW;
         }
     } else if(state == STATE_FILE) {
-        if(type == PIPE || type == PIPE_N) {
+        if(next_token_type == PIPE || next_token_type == PIPE_N) {
             return STATE_INIT;
-        } else if(type == NEW_LINE) {
+        } else if(next_token_type == NEW_LINE) {
             return STATE_NW;
         }
     }
     return -1;
+}
+
+
+void _insert_arg(arg_node_t** arg_list, arg_node_t* new_node) {
+    if(*arg_list == NULL) {
+        *arg_list = new_node;
+    } else {
+        arg_node_t* tmp_node = *arg_list;
+        while(tmp_node->next_node != NULL) {
+            tmp_node = tmp_node->next_node;
+        }
+        tmp_node->next_node = new_node;
+        new_node->next_node = NULL;
+    }
 }
--- parser.h
+++ parser.h
@@ -1,18 +1,20 @@
 #ifndef __PARSER_H__
 #define __PARSER_H__
-#include "node_list.h"
-#include "token.h"
+#include "cmd_node.h"
 
 // states
-
 #define STATE_INIT 0
 #define STATE_CMD 1
 #define STATE_FILE 2
 #define STATE_ARGS 3
 #define STATE_NW 4
 
-void parse(token_node_t* token_list, cmd_node_t** node_list);
+struct arg_node {
+    char* arg_str;
+    struct arg_node* next_node;
+};
 
-
+typedef struct arg_node arg_node_t;
+cmd_node_t* parse_tokens();
 
 #endif
